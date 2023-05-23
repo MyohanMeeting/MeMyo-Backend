ca8fce65b8cb0e1f625b09e69ea3bffe2b1bfb3b
DevelopJJong <jjong___e@naver.com>
2023-05-23 22:23:38 +0900

    스프링 부트 프로젝트 시작
    프로젝트 버전 : JAVA 17 / Spring 3.1.0
    
    들어간 초기 기술은 :
    
    *  Spring Web
    *  Spring Security
    *  Spring Validation
    *  DevTools
    *  Lombok
    *  H2

---
+++ MyohanMeeting/src/main/java/meet/myo/MyohanMeetingApplication.java
@@ -0,0 +1,13 @@
+package meet.myo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyohanMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyohanMeetingApplication.class, args);
+    }
+
+}
---
+++ MyohanMeeting/src/test/java/meet/myo/MyohanMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package meet.myo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyohanMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
