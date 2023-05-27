ea7949a8b614383374ed6069cc6beae1f2fea66e
BomLee427 <103720594+BomLee427@users.noreply.github.com>
2023-05-27 18:25:34 +0900

    style/ 주석 말머리 변경
    작업내용 설명을 위한 주석은 TODO와 구분하기 위해 MEMO라는 말머리로 변경했습니다.
    MEMO 역시 TODO나 FIXME와 같이 자동 색변경이 가능합니다. https://lsb530.github.io/2021/01/26/comment_cus/ 참조

--- MyohanMeeting/src/main/java/meet/myo/domain/cat/CatPictures.java
+++ MyohanMeeting/src/main/java/meet/myo/domain/cat/CatPictures.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import meet.myo.domain.Upload;
 
-//TODO: 실제 cat, upload 간의 관계를 rdb에서 표현하려면 중간 테이블이 존재해야 하므로 CatPictures라는 클래스로 풀어냈습니다.
+//MEMO: 실제 cat, upload 간의 관계를 rdb에서 표현하려면 중간 테이블이 존재해야 하므로 CatPictures라는 클래스로 풀어냈습니다.
 //  cat_pictures는 cat, upload의 외래키만 갖고 있는 연결 테이블입니다.
 @Entity
 @Getter
