21eb160ee9c7c0b1d1f2bc00a0c9f6388abe7354
BomLee427 <103720594+BomLee427@users.noreply.github.com>
2023-06-02 22:22:11 +0900

    feat: authority 관련 레포지토리 추가
    우선 Spring Data JPA 기본 레포지토리만 생성
    추후 Querydsl 필요 시 추가작업 요망

---
+++ MyohanMeeting/src/main/java/meet/myo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package meet.myo.repository;
+
+import meet.myo.domain.authority.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+}
