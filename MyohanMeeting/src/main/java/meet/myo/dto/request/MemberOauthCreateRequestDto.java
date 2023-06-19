e3bfea3e458cf097622bec6117a0164045ce490f
DevelopJJong <jjong___e@naver.com>
2023-06-19 22:44:34 +0900

    dto to entity 추가
    일단은 추가시켜둠 / 삭제 가능

--- MyohanMeeting/src/main/java/meet/myo/dto/request/MemberOauthCreateRequestDto.java
+++ MyohanMeeting/src/main/java/meet/myo/dto/request/MemberOauthCreateRequestDto.java
@@ -1,10 +1,20 @@
 package meet.myo.dto.request;
 
 import lombok.Getter;
+import meet.myo.domain.Member;
+import meet.myo.domain.OauthType;
 
 @Getter
 public class MemberOauthCreateRequestDto {
     private String oauthType;
     private String oauthId;
     private String email;
+
+    public Member toEntity() {
+        return Member.oauthJoinBuilder()
+                .email(this.email)
+                .oauthType(OauthType.valueOf(this.oauthType))
+                .oauthId(this.oauthId)
+                .build();
+    }
 }
