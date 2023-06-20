6009a5575abda99471a972833cc01852afb8521b
DevelopJJong <jjong___e@naver.com>
2023-06-21 02:31:08 +0900

    oauth2 login db저장하는 service (미완)

--- MyohanMeeting/src/main/java/meet/myo/service/CustomOauth2UserService.java
+++ MyohanMeeting/src/main/java/meet/myo/service/CustomOauth2UserService.java
@@ -3,8 +3,10 @@ package meet.myo.service;
 import jakarta.transaction.Transactional;
 import meet.myo.domain.Member;
 import meet.myo.domain.OauthType;
+import meet.myo.domain.exception.NotFoundException;
 import meet.myo.dto.request.MemberOauthCreateRequestDto;
 import meet.myo.repository.MemberRepository;
+import org.apache.el.stream.Optional;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -33,17 +35,24 @@ public class CustomOauth2UserService extends DefaultOAuth2UserService {
         String oauthId = (String) attributes.get("id");  // 카카오에서 제공하는 사용자 ID
         String email = (String) attributes.get("email");  // 카카오에서 제공하는 이메일
 
-        // DTO 생성
-        // TODO:저장하는 부분을 어떻게 처리할지 모르겠음..
-//        MemberOauthCreateRequestDto dto = new MemberOauthCreateRequestDto().toEntity();
-//                .oauthType(OauthType.KAKAOTALK)
-//                .oauthId(oauthId)
-//                .email(email)
-//                .build();
 
+        Member member = memberRepository.findByNickName(OauthType.KAKAOTALK + "_" + oauthId);
 
-        // 저장
-//        memberService.oauthJoin(dto.toEntity());
+        if (member == null){
+            Member newMember = Member.oauthJoinBuilder()
+                    .email(email)
+                    .oauthType(OauthType.KAKAOTALK)
+                    .oauthId(oauthId)
+                    .nickName(OauthType.KAKAOTALK + "_" + oauthId)
+                    .build();
+
+            newMember = memberRepository.save(newMember);
+        }
+        /**
+         *         TODO: JWT토큰 만드는 부분 어떻게 구현할지 모르겠음..
+         *
+          */
+
 
         return oAuth2User;
 
