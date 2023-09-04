59f63916c56cb2d27ebce63de895cf3b5271568d
BomLee427 <103720594+BomLee427@users.noreply.github.com>
2023-09-04 15:48:16 +0900

    fix: 문서화 description 추가

--- MyohanMeeting/src/main/java/meet/myo/dto/response/adopt/AdoptNoticeListResponseDto.java
+++ MyohanMeeting/src/main/java/meet/myo/dto/response/adopt/AdoptNoticeListResponseDto.java
@@ -8,8 +8,12 @@ import java.util.List;
 @Schema(name = "AdoptNoticeList")
 @Getter
 public class AdoptNoticeListResponseDto {
+    @Schema(description = "현재 검색 조건에 해당하는 공고의 총 갯수입니다.")
     private Long totalRows;
+
+    @Schema(description = "현재 검색 조건에 해당하는 공고의 총 페이지 수입니다.")
     private Long totalPages;
+
     private List<AdoptNoticeSummaryResponseDto> adoptNoticeList;
 
     public static AdoptNoticeListResponseDto of(long totalRows, long totalPages, List<AdoptNoticeSummaryResponseDto> adoptNoticeList) {
